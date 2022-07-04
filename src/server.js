c399dd93ac183259d1e148971a85bedac248b98c
Dom Harbot <domharbot@gmail.com>
2022-07-04 11:08:33 +0100

    Second commit

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require("express"); //importing entire express library
+const app = express(); //creating our app object, that houses our server instructions
+const port = process.env.PORT || 5001;
+
+app.use("/static", express.static("public")); //telling app to use the public folder when a request is made to /static
+
+app.listen(port, () =>{
+    console.log(`Listening on port ${port}`);
+});
+//listening on localhost:5001 for requests to our webserver
+
