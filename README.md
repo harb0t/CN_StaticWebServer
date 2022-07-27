dec5162678ac7a22ea624e8311c3ada53d252573
Dom Harbot <domharbot@gmail.com>
2022-07-27 21:27:10 +0100

    working now... code gods demanded href to be shortened

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# CN_StaticWebServer
+# Code nation Exercise: Static Web Server with ExpressJS
+
+## What is it?
+
+As on the tin it's a Static Web Server using  ExpressJS as part of bootcamp at Code Nation.
+
+## How do I run this?
+
+### Step 1
+run `npm install` to install the dependancies.
+
+### Step 2
+
+Run the server my typing into the terminal:
+`node src/server.js`
+
+### Step 3
+
+Open a web browser and enter the below in the URL bar
+
+> http://localhost:5001
+
+### Step 4
+
+Bask in the glory of this basic navbar.
--- src/server.js
+++ src/server.js
@@ -2,10 +2,13 @@ const express = require("express"); //importing entire express library
 const app = express(); //creating our app object, that houses our server instructions
 const port = process.env.PORT || 5001;
 
-app.use("/static", express.static("public")); //telling app to use the public folder when a request is made to /static
+// app.use("/static", express.static("public")); //telling app to use the public folder when a request is made to /static
+app.use("/", express.static("public"));
+app.use("/about", express.static("public/about.html"));
+app.use("/contact", express.static("public/contact.html"));
+app.use("/bibble", express.static("public/bibble.html"));
 
 app.listen(port, () =>{
     console.log(`Listening on port ${port}`);
 });
 //listening on localhost:5001 for requests to our webserver
-
